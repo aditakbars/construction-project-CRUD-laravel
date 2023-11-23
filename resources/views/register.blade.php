7871f75c873e82b0e024eb200a03308eae6988df
aditakbars <aditakbars33@gmail.com>
2023-11-23 21:44:36 +0700

    editing login and register form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -39,28 +39,12 @@
                         <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
                     </div>
                     <div class="form-group">
-                        <select class="form-control form-control-lg" id="exampleFormControlSelect2">
-                        <option>Country</option>
-                        <option>United States of America</option>
-                        <option>United Kingdom</option>
-                        <option>India</option>
-                        <option>Germany</option>
-                        <option>Argentina</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
                         <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="mb-4">
-                        <div class="form-check">
-                        <label class="form-check-label text-muted">
-                            <input type="checkbox" class="form-check-input"> I agree to all Terms & Conditions </label>
-                        </div>
                     </div>
                     <div class="mt-3">
                         <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN UP</a>
                     </div>
-                    <div class="text-center mt-4 font-weight-light"> Already have an account? <a href="login.html" class="text-primary">Login</a>
+                    <div class="text-center mt-4 font-weight-light"> Already have an account? <a href="/login" class="text-primary">Login</a>
                     </div>
                     </form>
                 </div>
