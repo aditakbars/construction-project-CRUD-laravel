d261650355954a468b202641a473864946265c63
aditakbars <aditakbars33@gmail.com>
2023-11-23 08:10:00 +0700

    adding restore function

--- resources/views/klien/trash.blade.php
+++ resources/views/klien/trash.blade.php
@@ -35,8 +35,8 @@
             <td>{{ $data->alamat }}</td>
             <td>{{ $data->industri }}</td>
             <td>
-                <a href="{{ route('klien.edit', $data->id_klien) }}" type="button"
-                    class="btn btn-warning rounded-3">Ubah</a>
+                <button type="button" class="btn btn-info" data-bs-toggle="modal"
+                    data-bs-target="#restoreModal{{ $data->id_klien }}">Pulihkan</button>
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                     data-bs-target="#hapusModal{{ $data->id_klien }}">
@@ -44,6 +44,30 @@
                 </button>
 
                 <!-- Modal -->
+                <div class="modal fade" id="restoreModal{{ $data->id_klien }}" tabindex="-1"
+                    aria-labelledby="restoreModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="restoreModalLabel">Konfirmasi</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close">
+                                </button>
+                            </div>
+                            <form method="POST" action="{{ route('klien.restore', $data->id_klien) }}">
+                                @csrf
+                                <div class="modal-body">
+                                    Apakah anda yakin ingin memulihkan data ini?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Tutup</button>
+                                    <button type="submit" class="btn btn-primary">Ya</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <div class="modal fade" id="hapusModal{{ $data->id_klien }}" tabindex="-1"
                     aria-labelledby="hapusModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
