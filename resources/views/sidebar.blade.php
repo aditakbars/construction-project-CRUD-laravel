16884ba4faa50d7360198e76c7fdadf867c09a12
aditakbars <aditakbars33@gmail.com>
2023-11-24 10:19:04 +0700

    fixing login and logout (success)

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -36,14 +36,21 @@
             <ul class="navbar-nav navbar-nav-right ml-auto">
                 <li class="nav-item dropdown d-none d-xl-inline-flex user-dropdown">
                 <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <img class="img-xs rounded-circle ml-2" src="{{ asset('images/faces/face8.jpg')}}" alt="Profile image"> <span class="font-weight-normal"> Henry Klein </span></a>
+                <img class="img-xs rounded-circle ml-2" src="https://source.unsplash.com/50x50?manager" alt="Profile image"> <span class="font-weight-normal">{{ Auth::user()->name }}</span></a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                     <div class="dropdown-header text-center">
-                        <img class="img-md rounded-circle" src="{{ asset('images/faces/face8.jpg')}}" alt="Profile image">
-                        <p class="mb-1 mt-3">Allen Moreno</p>
-                        <p class="font-weight-light text-muted mb-0">allenmoreno@gmail.com</p>
+                        <img class="img-md rounded-circle" src="https://source.unsplash.com/100x100?manager" alt="Profile image">
+                        <p class="mb-1 mt-3">{{ Auth::user()->name }}</p>
+                        <p class="font-weight-light text-muted mb-0">{{ Auth::user()->email }}</p>
                     </div>
-                    <a class="dropdown-item"><i class="dropdown-item-icon icon-power text-primary"></i>Sign Out</a>
+                    <form class="dropdown-item" method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();"><i class="dropdown-item-icon icon-power text-primary"></i>
+                            {{ __('Log Out') }}
+                        </a>
+                    </form>
                 </div>
                 </li>
             </ul>
