ddcad3186efb151b532ac37b7f05f7a2859daa72
aditakbars <aditakbars33@gmail.com>
2023-11-24 07:23:28 +0700

    fixing login and register views

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,38 +21,61 @@
     </head>
     <body>
         <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth">
-            <div class="row flex-grow">
-                <div class="col-lg-4 mx-auto">
-                <div class="auth-form-light text-left p-5">
-                    <div class="brand-logo">
-                    <img src="../../images/logo.svg">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="content-wrapper d-flex align-items-center auth">
+                    <div class="row flex-grow">
+                        <div class="col-lg-4 mx-auto">
+                            <div class="auth-form-light text-left p-5">
+                                <div class="brand-logo">
+                                    <img src="../../images/logo.svg">
+                                </div>
+                                <h4>Hello! let's get started</h4>
+                                <h6 class="font-weight-light">Sign in to continue.</h6>
+                                @if($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    @endif
+                                <form class="pt-3" action="/auth/login" method="POST">
+                                @csrf
+                                    <div class="form-group">
+                                        <label for="email"><h5>Email</h5></label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            class="form-control form-control-lg" 
+                                            id="email" 
+                                            value="{{ Session::get('email') }}"
+                                            placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password"><h5>Password</h5></label>
+                                        <input 
+                                            type="password" 
+                                            class="form-control form-control-lg" 
+                                            name="password" 
+                                            id="password" 
+                                            placeholder="Password">
+                                    </div>
+                                    <div class="mt-3">
+                                        <button type="submit" name="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
+                                    </div>
+                                    <!-- <div class="my-2 d-flex justify-content-between align-items-center">
+                                        <a href="#" class="auth-link text-black">Forgot password?</a>
+                                    </div> -->
+                                    <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="/register" class="text-primary">Create</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <h4>Hello! let's get started</h4>
-                    <h6 class="font-weight-light">Sign in to continue.</h6>
-                    <form class="pt-3">
-                    <div class="form-group">
-                        <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="mt-3">
-                        <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN IN</a>
-                    </div>
-                    <div class="my-2 d-flex justify-content-between align-items-center">
-                        <a href="#" class="auth-link text-black">Forgot password?</a>
-                    </div>
-                    <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="/register" class="text-primary">Create</a>
-                    </div>
-                    </form>
                 </div>
-                </div>
+                <!-- content-wrapper ends -->
             </div>
-            </div>
-            <!-- content-wrapper ends -->
-        </div>
         <!-- page-body-wrapper ends -->
         </div>
         <!-- container-scroller -->
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -21,38 +21,39 @@
     </head>
     <body>
         <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth">
-            <div class="row flex-grow">
-                <div class="col-lg-4 mx-auto">
-                <div class="auth-form-light text-left p-5">
-                    <div class="brand-logo">
-                    <img src="../../images/logo.svg">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="content-wrapper d-flex align-items-center auth">
+                    <div class="row flex-grow">
+                        <div class="col-lg-4 mx-auto">
+                            <div class="auth-form-light text-left p-5">
+                                <div class="brand-logo">
+                                    <img src="../../images/logo.svg">
+                                </div>
+                                <h4>New here?</h4>
+                                <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
+                                <form class="pt-3" action="/register" method="POST">
+                                @csrf
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <div class="mt-3">
+                                        <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN UP</a>
+                                    </div>
+                                    <div class="text-center mt-4 font-weight-light"> Already have an account? <a href="/login" class="text-primary">Login</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <h4>New here?</h4>
-                    <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
-                    <form class="pt-3">
-                    <div class="form-group">
-                        <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="mt-3">
-                        <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN UP</a>
-                    </div>
-                    <div class="text-center mt-4 font-weight-light"> Already have an account? <a href="/login" class="text-primary">Login</a>
-                    </div>
-                    </form>
                 </div>
-                </div>
+                <!-- content-wrapper ends -->
             </div>
-            </div>
-            <!-- content-wrapper ends -->
-        </div>
         <!-- page-body-wrapper ends -->
         </div>
         <!-- container-scroller -->
